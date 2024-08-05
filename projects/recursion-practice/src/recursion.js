5399667489ee322cbc86e257996fbe48fbd369a8
BenjaminCline17 <duckmancline@gmail.com>
2024-08-05 15:49:19 -0500

    Completed sumBelow

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -52,11 +52,27 @@ return true;
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  //base
+if (n === 0) {
+  return 0;
+}
+  //recursion
+if (n < 0) {
+  return n + 1 + sumBelow(n + 1);
+}
+
+  if (n > 0) {
+    return n - 1 + sumBelow(n - 1);
+  }
 };
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
 var range = function(x, y) {
+  //base
+
+  //recursion
+  
 };
 
 // 7. Compute the exponent of a number.

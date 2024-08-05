75c4b9d82d14472cc804625036d9e133abd8a037
BenjaminCline17 <duckmancline@gmail.com>
2024-08-05 15:37:09 -0500

    Completed tests 1, 2, and 4

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -4,21 +4,48 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
-var factorial = function(n) {
+var factorial = function(n, product=1) {
+  //base
+if (n < 0){
+  return null;
+}
+ if(n === 1 || n === 0){
+ return product;
+ }
+  //recursion
+ product *= n;
+
+ return factorial(n - 1, product);
 };
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  //base
+if (array.length === 0){
+ return 0;
+}
+  //recursion
+  return array[0] + sum(array.slice(1));
 };
 
-// 3. Sum all numbers in an array containing nested arrays.
+// 3. Sum all numbers in an array containing nested arrays. Optional
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  //base
+ if(n === 0) {
+return true;
+ }
+ if(n === 1) {
+  return false;
+ }
+  //recursion
+  return isEven(Math.abs(n) - 2);
 };
 
 // 5. Sum all integers below a given integer.
